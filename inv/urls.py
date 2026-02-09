3bf334620b9956c29c9a7bc3a5c425a361ff875c
hjmx32 <franco.daniel.perez@aptiv.com>
2026-02-09 19:35:18 +0000

    Update urls.py

--- inv/urls.py
+++ inv/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
-from .views import InventoryListView, inventory_create, inventory_edit, inventory_delete, user_create, HomeView, register, log_out
+from .views import InventoryListView, inventory_create, inventory_edit, inventory_delete, user_create, HomeView, log_out
 
 app_name = "inv"
 urlpatterns = [
@@ -10,7 +10,6 @@ urlpatterns = [
     path("<int:pk>/edit/", inventory_edit, name="edit"),
     path("usr_create/", user_create, name='usr_create'),
     path("<int:pk>/delete/", inventory_delete, name="delete"),
-    #path('register/', register, name= "register"),
     path('login/', auth_views.LoginView.as_view(template_name="inv/login.html"), name='login'),
     path('logout/', log_out, name='logout'),
 ]
